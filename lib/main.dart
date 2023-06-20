9e46ee3638d3953fa51022073c04712697fa34d7
UTKARSH SINGH <us9766134@gmail.com>
2023-06-21 00:41:35 +0530

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,30 @@
+import 'package:employee_attendance_system/screens/loginscreen.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
+import 'package:supabase_flutter/supabase_flutter.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  //load env //initialising the supabase
+  await dotenv.load();
+  String supabaseUrl = dotenv.env['SUPABASE_URL'] ?? '';
+  String supabasekey = dotenv.env['SUPABASE_KEY'] ?? '';
+  await Supabase.initialize(url: supabaseUrl, anonKey: supabasekey);
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+        debugShowCheckedModeBanner: false,
+        title: 'Flutter Demo',
+        theme: ThemeData(
+          primarySwatch: Colors.blue,
+        ),
+        home: const LoginScreen());
+  }
+}

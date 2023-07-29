e91b65b1ae1ff15fc0dd58bcbaa101f31894601b
UTKARSH SINGH <us9766134@gmail.com>
2023-07-29 23:32:18 +0530

    complete project updated

--- lib/services/db_services.dart
+++ lib/services/db_services.dart
@@ -1,5 +1,7 @@
 import 'package:employee_attendance_system/constants/constants.dart';
+import 'package:employee_attendance_system/models/department_model.dart';
 import 'package:employee_attendance_system/models/user_model.dart';
+import 'package:employee_attendance_system/utils/utils.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/rendering.dart';
 import 'package:supabase_flutter/supabase_flutter.dart';
@@ -8,6 +10,8 @@ import 'dart:math';
 class DBService extends ChangeNotifier {
   final SupabaseClient _supabase = Supabase.instance.client;
   UserModel? userModel;
+  List<DepartmentModel> alldepartments = [];
+  int? employeeDepartment;
 
   String generateRandomEmployeeId() {
     final random = Random();
@@ -37,8 +41,31 @@ class DBService extends ChangeNotifier {
         .select()
         .eq('id', _supabase.auth.currentUser!.id)
         .single();
-    userModel = UserModel.fromJson(
-        userData); //getting data from database in json and store into variable usermodel
+    userModel = UserModel.fromJson(userData);
+    //getting data from database in json and store into variable usermodel
+    employeeDepartment == null
+        ? employeeDepartment = userModel?.department
+        : null;
     return userModel!;
+  }
+
+  Future<void> getAllDepartments() async {
+    final List result =
+        await _supabase.from(Constants.departmentTable).select();
+    alldepartments = result
+        .map((department) => DepartmentModel.fromJson(department))
+        .toList();
+    notifyListeners();
+  }
+
+  Future updateProfile(String name, BuildContext context) async {
+    await _supabase.from(Constants.employeeTable).update({
+      'name': name,
+      'department': employeeDepartment,
+    }).eq('id', _supabase.auth.currentUser!.id);
+
+    Utils.showSnackBar("Profile Updated Successfully", context,
+        color: Colors.green);
+    notifyListeners();
   }
 }
